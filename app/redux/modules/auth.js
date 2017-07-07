366fa38ca786f8fca2cf62a3ece8114f4e66c9dc
Tonida Baraza <tonida.baraza@andela.com>
2017-07-07 17:23:51 +0300

    add signup redux  module

---
+++ app/redux/modules/auth.js
@@ -0,0 +1,26 @@
+import types from '../actionTypes';
+
+const DEFAULT_STATE = {
+    email: '',
+    password: '',
+};
+
+export function authUser(user) {
+    return {
+        type: types.SIGNUP_REQUEST_SUCCESS,
+        payload: user
+    };
+}
+
+export default function reducer(state = DEFAULT_STATE, action = {}) {
+    switch (action.type) {
+    case types.SIGNUP_REQUEST_SUCCESS:
+        return Object.assign({}, state, {
+         email: action.payload.email,
+         password: action.payload.password
+      });
+
+    default:
+        return state;
+    }
+}
