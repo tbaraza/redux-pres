0fdc0c72bd53e1454db199ffa702aa6c79e37bd5
Tonida Baraza <tonida.baraza@andela.com>
2017-08-03 19:34:27 +0300

    add selector and action

--- app/redux/modules/auth.js
+++ app/redux/modules/auth.js
@@ -3,15 +3,28 @@ import types from '../actionTypes';
 const DEFAULT_STATE = {
     email: '',
     password: '',
+    data: {},
 };
 
+/* selector */
+export function getEmail(state) {
+    return state.auth.email;
+}
 
-export function authUser(user) {
+/* action creators */
+export function signupRequest() {
+    return {
+        type: types.SIGN_UP_REQUEST,
+    };
+}
+
+export function signupRequestSuccess(user) {
     return {
         type: types.SIGNUP_REQUEST_SUCCESS,
         payload: user,
     };
 }
+
 
 export default function reducer(state = DEFAULT_STATE, action = {}) {
     switch (action.type) {
@@ -21,8 +34,28 @@ export default function reducer(state = DEFAULT_STATE, action = {}) {
             email: action.payload.email,
             password: action.payload.password,
         };
+    case types.GET_STARSHIPS_SUCCESS:
+        return {
+            ...state,
+            data: action.payload.data,
+        };
+
+    case types.GET_STARSHIPS_FAIL:
+        return {
+            ...state,
+            errors: action.payload,
+        };
 
     default:
         return state;
     }
+}
+
+export function signUp(email, password) {
+    return (dispatch) => {
+        dispatch(signupRequest());
+        return setTimeout(
+            dispatch(signupRequestSuccess({ email, password }),
+            1000));
+    };
 }
