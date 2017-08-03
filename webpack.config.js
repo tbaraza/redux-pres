00c01ab57d827406359ccb0a86ca5b69984364cb
Tonida Baraza <tonida.baraza@andela.com>
2017-08-03 19:24:13 +0300

    add css and sass loaders to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,39 @@
+const combineLoaders = require('webpack-combine-loaders');
 const path = require('path');
 
 module.exports = {
-  entry: path.join(__dirname, 'app/index.js'),
-  output: {
-    path: path.join(__dirname, '/dist/'),
-    filename: 'bundle.js',
-    publicPath: '/'
-  },
-  module: {
-    loaders: [{
-      test: /\.jsx?$/,
-      exclude: /node_modules/,
-      loader: 'babel-loader',
-      query: {
-        presets: ['react', 'es2015', 'stage-0']
-      }
-    }]
-  }
+    entry: path.join(__dirname, 'app/index.js'),
+    output: {
+        path: path.join(__dirname, '/dist/'),
+        filename: 'bundle.js',
+        publicPath: '/',
+    },
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+                presets: ['react', 'es2015', 'stage-0'],
+            },
+        },
+        {
+            test: /\.scss$/,
+            loader: combineLoaders([
+                {
+                    loader: 'style-loader',
+                },
+                {
+                    loader: 'css-loader',
+                    query: {
+                        modules: true,
+                        localIdentName: '[name]__[local]___[hash:base64:5]',
+                    },
+                },
+                {
+                    loader: 'sass-loader',
+                }
+            ]),
+        }],
+    },
 };
