fcd1be468e7287ec8cbcb838d23710d9b479a848
Tonida Baraza <tonida.baraza@andela.com>
2017-08-03 19:32:47 +0300

    add saga and thunk middleware to the store

--- app/redux/store/configureStore.js
+++ app/redux/store/configureStore.js
@@ -1,13 +1,31 @@
 import { applyMiddleware, compose, createStore } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import getStarshipsSaga from '../sagas/starshipsSaga';
 import rootReducer from './rootReducer';
+import getInitialData from './defaultState';
 
 export default function configureStore() {
+    // create the saga middleware
+    const sagaMiddleware = createSagaMiddleware();
+    const initialState = getInitialData();
+    console.log('initialState', initialState);
+
     const enhancer = compose(
-        applyMiddleware(logger),
+        applyMiddleware(
+            logger,
+            sagaMiddleware,
+            thunk
+        ),
         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     );
+
     const store = createStore(rootReducer, enhancer);
+
+    // then run the saga
+    sagaMiddleware.run(getStarshipsSaga);
+
     return store;
 }
 
